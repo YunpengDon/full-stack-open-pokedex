94fc7f3ed4a26edc6817fb16579be77191c1d775
Yunpeng Dong <58433153+YunpengDon@users.noreply.github.com>
2025-02-03 21:00:13 +0800

    Simple end-to-end tests

--- e2e-tests/example.spec.js
+++ e2e-tests/example.spec.js
@@ -2,7 +2,7 @@ const { test, describe, expect } = require('@playwright/test')
 
 describe('Pokedex', () => {
   test('front page can be opened', async ({ page }) => {
-    await new Promise(resolve => setTimeout(resolve, 1000));
+    await new Promise(resolve => setTimeout(resolve, 1000))
     await page.goto('')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
